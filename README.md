a549fef854032356313fb2ff01b5504fc099016e
Abhishek Verma <abhishekverma3124@gmail.com>
2021-10-27 18:44:46 +0530

    :bug: Fixed Google unreachable issue

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # KeywordLookupCLI
 KeywordLookupCLI is a command line tool for keyword research.
+
+## To-Do
+- [ ] Replace BS4 with Selenium
--- finder.py
+++ finder.py
@@ -4,8 +4,16 @@ import requests
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15'}
 
 postKeyword = input('Enter a keyword for keyword research: ')
-gQuery = 'https://www.google.com/?q='% postKeyword
-
-response = requests.get(gQuery)
-
-print('Request sent! Waiting for response.')
+gQuery = 'https://www.ask.com/web?q=%s&ad=dirN&qo=homepageSearchBox' % str(postKeyword)
+# replaced Google with Ask because Google is blocked
+response = requests.get(gQuery, headers=headers)
+html_response = response.text
+# print(html_response)
+soup = BeautifulSoup(html_response, 'html.parser')
+relresponse = soup.find_all("span", class_="PartialRelatedSearch-item-link-text")
+print("Related Keywords for your Post Idea")
+if len(relresponse) == None:
+    print("No Related Keywords Found")
+else:
+    for i in range(len(relresponse)//2):
+        print(relresponse[i].text)
